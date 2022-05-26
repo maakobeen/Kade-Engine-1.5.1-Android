177ebe2fd4afa4c67c9af4b54fed5e2ea708503c
maakobeen <106302765+maakobeen@users.noreply.github.com>
2022-05-26 16:40:34 +0700

    Add files via upload

---
+++ assets/preload/data/freaky-machine/script.lua
@@ -0,0 +1,32 @@
+function onStartCountdown()
+	if not allowCountdown then
+		runTimer('startText', 0.1);
+		allowCountdown = true;
+		startCountdown();
+		return Function_Stop;
+	end
+	return Function_Continue;
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	if tag == 'startText' then
+		makeLuaSprite('BlackScreen', 'BlackScreen', 0, 0);
+		setObjectCamera('BlackScreen', 'hud');
+		addLuaSprite('BlackScreen', true);
+		makeLuaSprite('circle', 'Circle', 777, 0);
+		setObjectCamera('circle', 'hud');
+		addLuaSprite('circle', true);
+		makeLuaSprite('text', 'introductionbonus', 0, 0);
+		setObjectCamera('text', 'hud');
+		addLuaSprite('text', true);
+		runTimer('appear', 0.6, 1);
+		runTimer('fadeout', 1.9, 1);
+	elseif tag == 'appear' then
+		doTweenX('circletween', 'circle', 0, 0.5, 'linear');
+		doTweenX('texttween', 'text', 0, 0.5, 'linear');
+	elseif tag == 'fadeout' then
+		doTweenAlpha('circlefade', 'circle', 0, 1, 'linear');
+		doTweenAlpha('textfade', 'text', 0, 1, 'linear');
+		doTweenAlpha('blackfade', 'BlackScreen', 0, 1, 'linear');
+	end
+end
